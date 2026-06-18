78b5f4eeef60fc4486c21ead1ceb5060b790d34c
Ikedi-21 <ikedinachiogbonne@gmail.com>
2026-06-18 16:09:21 +0100

    added a function to calculate perimeter and test it

--- calculate_area.py
+++ calculate_area.py
@@ -1,2 +1,5 @@
 def calculate_area(length, breadth):
   return length * breadth
+
+def calculate_perimeter(length, breadth):
+  return 2 * (length + breadth)
--- test_calculate.py
+++ test_calculate.py
@@ -1,4 +1,7 @@
-from calculate_area import calculate_area
+from calculate_area import calculate_area, calculate_perimeter
 
 def test_area():
   assert calculate_area(5, 4) == 20
+
+def test_perimeter():
+  assert calculate_perimeter(5, 4) == 18
